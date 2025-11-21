2c9b17e3eb10e15a0a485260aa1244ff4be1fe28
folajindayo <temidayofolajin@yahoo.com>
2025-11-21 13:03:16 +0100

    chore: add services barrel export

---
+++ apps/wallet-health/src/application/services/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Services Barrel Export
+ */
+
+export { securityAnalysisService } from './security-analysis.service';
+export { notificationService } from './notification.service';
+export { alertService } from './alert.service';
+export { monitoringService } from './monitoring.service';
+
