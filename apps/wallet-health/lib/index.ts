9d54e375cc6d40528071ac1dafa79325e8aa8c65
folajindayo <temidayofolajin@yahoo.com>
2025-11-21 08:18:00 +0100

    feat(wallet-health): add main library exports

---
+++ apps/wallet-health/lib/index.ts
@@ -0,0 +1,19 @@
+/**
+ * Main Library Exports
+ */
+
+// Services
+export * from './services/approval.service';
+export * from './services/threat.service';
+
+// Utilities
+export * from './utils/risk-scorer';
+export * from './utils/security-calculator';
+
+// Constants
+export * from './constants/security-levels';
+export * from './constants/risk-thresholds';
+
+// Types
+export * from './types/approval.types';
+
